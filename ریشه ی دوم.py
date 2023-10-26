ac0ac34d93129e599b83cbaa74f0c32c7b4a55c4
keyvan_rahban <107574965+Keyvanrahban@users.noreply.github.com>
2023-10-26 18:06:04 +0330

    ریشه ی دوم
    ریشه ی دوم

---
+++ ریشه ی دوم.py
@@ -0,0 +1,9 @@
+from math import sqrt
+
+number_of_inputs = int(input())
+sqrts = []
+for i in range(0, number_of_inputs):
+    sqrts.append(sqrt(float(input())))
+
+for i in sqrts:
+    print("%.4f" % (int(i * 10000)/10000))
