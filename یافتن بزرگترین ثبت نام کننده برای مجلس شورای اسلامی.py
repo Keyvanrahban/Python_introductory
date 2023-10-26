9347b93305ec7427ecd12a294f2e3a36c8fff94f
keyvan_rahban <107574965+Keyvanrahban@users.noreply.github.com>
2023-10-26 17:46:46 +0330

    یافتن بزرگترین ثبت نام کننده برای مجلسشورای اسلامی
    یافتن بزرگترین ثبت نام کننده برای مجلس شورای اسلامی

---
+++ یافتن بزرگترین ثبت نام کننده برای مجلس شورای اسلامی.py
@@ -0,0 +1,16 @@
+b = int(input())
+a = int(input())
+maxi = 0
+maxi2 = 0
+if a > b:
+    maxi = a
+    maxi2 = b
+else:
+    maxi = b
+    maxi2 = a
+while b != -1:
+    b = int(input())
+    if b > maxi:
+        maxi2 = maxi
+        maxi = b
+print(maxi)
